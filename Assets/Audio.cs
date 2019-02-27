d3afd33c4f73e58c0db0563a9de659874a06e900
fumi uk <dcfumiya.238@gmail.com>
2019-02-28 05:47:29 +0900

    GoForwardUnityChan・課題
    2Dゲームの課題について、cubePrefab同士の衝突時に効果音が鳴るようになりました。

---
+++ Assets/Audio.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour {
+
+    private AudioSource audioSource;
+
+    public AudioClip sound1;
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "block")
+        {
+            audioSource.PlayOneShot(sound1);
+        }
+    }
+}
